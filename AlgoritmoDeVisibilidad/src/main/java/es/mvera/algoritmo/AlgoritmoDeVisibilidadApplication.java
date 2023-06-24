abbd03e780297f3fa61a6e3f73105b209d3700c9
Manuel Vera Silvestre <manversil@gmail.com>
2023-06-24 10:15:16 +0200

    Commit inicial: nuevo proyecto basado en Spring Boot

---
+++ AlgoritmoDeVisibilidad/src/main/java/es/mvera/algoritmo/AlgoritmoDeVisibilidadApplication.java
@@ -0,0 +1,13 @@
+package es.mvera.algoritmo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgoritmoDeVisibilidadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgoritmoDeVisibilidadApplication.class, args);
+	}
+
+}
---
+++ AlgoritmoDeVisibilidad/src/test/java/es/mvera/algoritmo/AlgoritmoDeVisibilidadApplicationTests.java
@@ -0,0 +1,13 @@
+package es.mvera.algoritmo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgoritmoDeVisibilidadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
