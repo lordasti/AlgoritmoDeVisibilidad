77a9df4a4219e5d10371fb38568f2e86c2c8ecc0
Manuel Vera Silvestre <manversil@gmail.com>
2023-06-24 17:34:40 +0200

    Añadidos los diferentes casos de prueba que debe superar el algoritmo

---
+++ AlgoritmoDeVisibilidad/src/test/java/es/mvera/algoritmo/AlgoritmoDeVisibilidadTest.java
@@ -0,0 +1,51 @@
+package es.mvera.algoritmo;
+
+import org.junit.jupiter.api.Test;
+
+public class AlgoritmoDeVisibilidadTest {
+
+	@Test
+	public void unProductoNoEsVisibleCuandoNoTieneTallasEspecialesNiNingunaTallaVuelveProntoNiNingunaTallaTieneStock() throws Exception {
+		
+	}
+	
+	@Test
+	public void deberiaDevolverUnProductoCuandoNoTieneTallasEspecialesNiNingunaTallaVuelveProntoPeroAlgunaTallaTieneStock() throws Exception {
+		
+	}
+	
+	@Test
+	public void deberiaDevolverUnProductoCuandoNoTieneTallasEspecialesPeroAlgunaTallaVuelvePronto() throws Exception {
+		
+	}
+	
+	@Test
+	public void unProductoNoEsVisibleCuandoTieneTallasEspecialesYNoHayNingunaTallaNoEspecialQueVuelvaProntoNiTengaStock() throws Exception {
+		
+	}
+	
+	@Test
+	public void unProductoNoEsVisibleCuandoTieneTallasEspecialesYNoHayNingunaTallaEspecialQueVuelvaProntoNiTengaStock() throws Exception {
+		
+	}
+	
+	@Test
+	public void deberiaDevolverUnProductoCuandoTieneTallasEspecialesYHayStockTantoDeAlgunaTallaEspecialComoDeAlgunaTallaNoEspecial() throws Exception {
+		
+	}
+	
+	@Test
+	public void deberiaDevolverUnProductoCuandoTieneTallasEspecialesYHayStockDeAlgunaTallaEspecialYNoHayStockDeNingunaTallaNoEspecialPeroAlgunaVuelvePronto() throws Exception {
+		
+	}
+	
+	@Test
+	public void deberiaDevolverUnProductoCuandoTieneTallasEspecialesYNoHayStockDeNingunaTallaEspecialPeroAlgunaVuelveProntoYHayStockDeAlgunaTallaNoEspecial() throws Exception {
+		
+	}
+	
+	@Test
+	public void deberiaDevolverUnProductoCuandoTieneTallasEspecialesYVuelvenProntoTantoAlgunaTallaEspecialComoAlgunaTallasNoEspecial() throws Exception {
+		
+	}
+}
