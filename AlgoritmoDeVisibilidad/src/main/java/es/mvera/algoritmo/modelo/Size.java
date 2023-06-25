2ce5f39130963749c498d026479e3049a455272f
Manuel Vera Silvestre <manversil@gmail.com>
2023-06-25 10:55:08 +0200

    Asegurada la persistencia de Size

--- AlgoritmoDeVisibilidad/src/main/java/es/mvera/algoritmo/modelo/Size.java
+++ AlgoritmoDeVisibilidad/src/main/java/es/mvera/algoritmo/modelo/Size.java
@@ -2,24 +2,41 @@ package es.mvera.algoritmo.modelo;
 
 import java.util.Optional;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class Size {
 	
 	@Id
-	private final Integer id;
-	private final Integer productId;
-	private final boolean backSoon;
-	private final boolean special;
-	private final Stock stock;
+	@Column(name="ID")
+	private  Integer id;
+	
+	@Column(name="PRODUCTID")
+	private  Integer productId;
+	
+	@Column(name="BACKSOON")
+	private  boolean backSoon;
+	
+	@Column(name="SPECIAL")
+	private  boolean special;
+	
+	@OneToOne
+	@MapsId
+	@JoinColumn(name = "ID")
+	private  Stock stock;
 	
 	public boolean tieneStock() {
 		return Optional.ofNullable(stock).map(Stock::hayStock).orElse(false);
--- AlgoritmoDeVisibilidad/src/test/java/es/mvera/algoritmo/repositorio/RepositorioSizeTest.java
+++ AlgoritmoDeVisibilidad/src/test/java/es/mvera/algoritmo/repositorio/RepositorioSizeTest.java
@@ -6,13 +6,28 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import es.mvera.algoritmo.modelo.Size;
+
 @DataJpaTest
 public class RepositorioSizeTest {
+	
 	@Autowired
     private RepositorioSize repositorioSize;
  
     @Test
-    void deberiaDevolverElNumeroDeProductosDelRepositorio() {
+    public void deberiaDevolverElNumeroDeProductosDelRepositorio() {
         assertEquals(17, repositorioSize.count());        
+    }
+    
+    @Test
+    public void deberiaRecuperarCorrectamenteElObjetoDeLaBD() {
+    	Size size = repositorioSize.findById(11).orElseThrow();
+    	        
+        assertEquals(11, size.getId());
+        assertEquals(1, size.getProductId());
+        assertEquals(true, size.isBackSoon());
+        assertEquals(false, size.isSpecial());
+        assertEquals(11, size.getStock().getSizeId());
+        assertEquals(0, size.getStock().getQuantity());
     }
 }
