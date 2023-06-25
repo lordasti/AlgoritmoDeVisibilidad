9d5b823cee9d4ddd7278d659304cc98942dbb4c3
Manuel Vera Silvestre <manversil@gmail.com>
2023-06-25 12:39:56 +0200

    Errata en el nombre de la clase de test de Producto

--- AlgoritmoDeVisibilidad/src/test/java/es/mvera/algoritmo/modelo/ProductTest.java
+++ AlgoritmoDeVisibilidad/src/test/java/es/mvera/algoritmo/modelo/ProductTest.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
-public class ProductoTest {
+public class ProductTest {
 	@Test
 	public void deberiaIndicarQueElProductoTieneTallasEspecialesCuandoAlgunaDeLasTallasEsEspecial() throws Exception {
 		Size tallaEspecial = Size.builder().special(true).build();
