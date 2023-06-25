f926dbc6bf0adf506670336b5791b2a5cf029054
Manuel Vera Silvestre <manversil@gmail.com>
2023-06-25 12:36:56 +0200

    Implementación del método que resuelve el problema propuesto

--- AlgoritmoDeVisibilidad/src/main/java/es/mvera/algoritmo/AlgoritmoDeVisibilidad.java
+++ AlgoritmoDeVisibilidad/src/main/java/es/mvera/algoritmo/AlgoritmoDeVisibilidad.java
@@ -1,11 +1,18 @@
 package es.mvera.algoritmo;
 
+import java.util.Comparator;
+import java.util.List;
+
 import es.mvera.algoritmo.modelo.Product;
 
 
 public class AlgoritmoDeVisibilidad {
 	
-	public static boolean esVisible(Product producto) {
+	public static List<Product> obtenerListaDeProductosVisibles(List<Product> productos) {
+		return productos.stream().filter(AlgoritmoDeVisibilidad::esVisible).sorted(Comparator.comparing(Product::getSequence)).toList();
+	}
+	
+	protected static boolean esVisible(Product producto) {
 		if((!producto.tieneTallasEspeciales() && !producto.algunaTallaVuelvePronto() && !producto.algunaTallaTieneStock())
 			|| (producto.tieneTallasEspeciales() && !producto.algunaTallaNoEspecialVuelvePronto() && !producto.algunaTallaNoEspecialTieneStock())
 			|| (producto.tieneTallasEspeciales() && !producto.algunaTallaEspecialVuelvePronto() && !producto.algunaTallaEspecialTieneStock())) {
